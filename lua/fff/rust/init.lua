4b31ac831131547308b149e0464f70b77cf73be2
Agung Baptiso Sorlawan <tisorlawan@gmail.com>
2025-08-04 14:31:24 +0700

    fix: fail to load if `CARGO_TARGET_DIR` is set (#39)

--- lua/fff/rust/init.lua
+++ lua/fff/rust/init.lua
@@ -8,14 +8,18 @@ end
 -- search for the lib in the /target/release directory with and without the lib prefix
 -- since MSVC doesn't include the prefix
 local base_path = debug.getinfo(1).source:match('@?(.*/)')
-package.cpath = package.cpath
-  .. ';'
-  .. base_path
-  .. '../../../target/release/lib?'
-  .. get_lib_extension()
-  .. ';'
-  .. base_path
-  .. '../../../target/release/?'
-  .. get_lib_extension()
+
+local paths = {
+  base_path .. '../../../target/release/lib?' .. get_lib_extension(),
+  base_path .. '../../../target/release/?' .. get_lib_extension(),
+}
+
+local cargo_target_dir = os.getenv('CARGO_TARGET_DIR')
+if cargo_target_dir then
+  table.insert(paths, cargo_target_dir .. '/release/lib?' .. get_lib_extension())
+  table.insert(paths, cargo_target_dir .. '/release/?' .. get_lib_extension())
+end
+
+package.cpath = package.cpath .. ';' .. table.concat(paths, ';')
 
 return require('fff_nvim')
